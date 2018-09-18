2bab42c2c53609f911cded22b41e8d203d15310b
Manny Pissanos <mpissano@gmail.com>
2018-09-18 21:30:57 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(arr)
+  if block_given?
+  i = 0
+  
+  while i < arr.length
+   yield arr[i]
+   i = i + 1
+  end
+end
 
+# my_collect(students) do |name|
+#   name.split(" ").first 
+# end
 
+# my_collect(languages) do |lang|
+#   lang.upcase
+# end 
