152f257cc8c91be26699ab885eb87e978802855d
Manny Pissanos <mpissano@gmail.com>
2018-09-19 16:25:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,17 +1,20 @@
 def my_collect(array)
   i = 0
-  
+  new_arr = []
   while i < array.length
-    yield(array[i])
+    new_arr << yield(array[i])
     i = i + 1
   end
+  new_arr
 end
 
 
-my_collect(students) do |student|
-  puts student.split(" ").first
-end
+# my_collect(students) do |student|
+#   puts student.split(" ").first
+# end
 
-my_collect(collection) do |language|
- puts language.upcase
-end
+# my_collect(collection) do |language|
+# puts language.upcase
+# end
+
+
